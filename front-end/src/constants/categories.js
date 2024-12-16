e45315f45df2bca5d10b07608ae0d362ce538a7f
mslysz <macslysz@gmail.com>
2024-12-16 04:39:35 +0000

    added CategoryMenu component and styles

--- front-end/src/constants/categories.js
+++ front-end/src/constants/categories.js
@@ -12,3 +12,19 @@ export const GENDERS = [
     path: 'dziecko',
   },
 ];
+
+export const CATEGORIES = [
+  {
+    categoryName: 'Odzież',
+    path: 'odziez',
+  },
+  {
+    categoryName: 'Obuwie',
+    path: 'obuwie',
+  },
+  {
+    categoryName: 'Akcesoria',
+    path: 'akcesoria',
+  },
+  { categoryName: 'Sport', path: 'sport' },
+];
