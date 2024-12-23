d9407c1fba7a849db0848a863697951fadb29fd2
mslysz <macslysz@gmail.com>
2024-12-23 05:33:33 +0000

    Added Photos, Details, Accordion components

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -4,12 +4,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import { ProductsList } from './views/ProductsList/ProductsList.jsx';
+import { ProductDetails } from './views/ProductDetails/ProductDetails.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <ProductsList />,
+    element: <ProductDetails />,
   },
 ]);
 
