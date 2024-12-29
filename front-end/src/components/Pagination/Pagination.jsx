2f065088f0b783fb09f6ce28cc9e0be7a128d7a6
mslysz <macslysz@gmail.com>
2024-12-29 04:29:14 +0000

    added Pagination and Breadcrumbs functionality

--- front-end/src/components/Pagination/Pagination.jsx
+++ front-end/src/components/Pagination/Pagination.jsx
@@ -1,15 +1,14 @@
 import styles from './Pagination.module.css';
-import { NavLink } from 'react-router-dom';
 
+import { NavLink } from 'react-router-dom';
 export function Pagination({ numberOfPages }) {
   const pages = Array(numberOfPages).fill(null);
-
   return (
     <ul className={styles.pagination}>
       {pages.map((page, index) => {
         return (
           <li key={index}>
-            <NavLink>{index + 1}</NavLink>
+            <NavLink to={`?page=${index + 1}`}>{index + 1}</NavLink>
           </li>
         );
       })}
